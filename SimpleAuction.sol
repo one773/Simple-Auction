def2c14ab4fbb55a129ab44e9f257cbc05886b29
one773 <104087116+one773@users.noreply.github.com>
2024-05-26 17:17:14 +0200

    Create SimpleAuction.sol

---
+++ SimpleAuction.sol
@@ -0,0 +1,43 @@
+FILE EXPLORER
+
+33343536372627282930313217181920212223242589101112131415161234567
+// SIMPLE AUCTION 
+
+// 1. Starting an Auction
+// 2. Bidding
+// 3. Ending an Auction
+// 4. Withdrawals
+// 5. Claiming item.
+
+// SPDX-License-Identifier:MIT
+
+…        return highestBid;
+    }
+
+    function getHighestBidder() public view returns (address) {
+        return highestBidder;
+    }
+
+
+}
+0
+Listen on all transactions
+Filter with transaction hash or address
+ Welcome to Remix 0.49.0 
+
+Your files are stored in indexedDB, 11.1 MB / 2 GB used
+
+You can use this terminal to: 
+Check transactions details and start debugging.
+Execute JavaScript scripts:
+ - Input a script directly in the command line interface 
+ - Select a Javascript file in the file explorer and then run `remix.execute()` or `remix.exeCurrent()`  in the command line interface 
+ - Right click on a JavaScript file in the file explorer and then click `Run` 
+The following libraries are accessible:
+web3.js
+ethers.js 
+gpt <your question here>  
+sol-gpt <your Solidity question here>  
+Type the library name to see available commands.
+Solidity copilot not activated!
+>
